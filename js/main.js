1779bfd166e8bd89188c1b30d018ddc3eac503c2
von Schappler <vonschappler.gaming@users.noreply.github.com>
2018-08-02 08:01:34 -0300

    1st upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+function landing() {
+	var home = './pages/home.html';
+	//var home = '/pages/home.html'; //github
+	$('#page').load(home);
+	//$(document).setTitle("von Schappler Gaming - HOME");
+	//alert("Carregando " + page + " no link\n" + home);
+	return false;
+}
+
+	function loadPages() {
+		$('.item').click (function () {
+			var page = $(this).attr('href');
+			var link = './pages/' + page + '.html';
+			//var link = '/pages/' + page + '.html'; //gitub
+			$('#page').load(link);
+			return false;
+		});
+	}
+
+
