9047deb49651f44721bd8ffbfd13ce25a09148f5
von Schappler <vonschappler.gaming@users.noreply.github.com>
2018-08-06 12:52:30 -0300

    UpdateScript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // JavaScript Document
 
 function landing() {
-	var home = './pages/home.html';
-	//var home = '/pages/home.html'; //github
+	//var home = './pages/home.html';
+	var home = '/pages/home.html'; //github
 	$('#page').load(home);
-	//$(document).setTitle("von Schappler Gaming - HOME");
+	$(document).setTitle("von Schappler Gaming - HOME");
 	//alert("Carregando " + page + " no link\n" + home);
 	return false;
 }
@@ -12,8 +12,8 @@ function landing() {
 	function loadPages() {
 		$('.item').click (function () {
 			var page = $(this).attr('href');
-			var link = './pages/' + page + '.html';
-			//var link = '/pages/' + page + '.html'; //gitub
+			//var link = './pages/' + page + '.html';
+			var link = '/pages/' + page + '.html'; //gitub
 			$('#page').load(link);
 			return false;
 		});
