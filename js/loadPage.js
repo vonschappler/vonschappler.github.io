81e255781b248c45b77a5074f1287f5098fcd1ee
von Schappler <vonschappler.gaming@users.noreply.github.com>
2018-08-19 17:24:11 -0300

    teste2

--- js/loadPage.js
+++ js/loadPage.js
@@ -5,7 +5,15 @@ $(document).ready(function() {
 	
 	//load page by click
 	$('.item').click(function() {
+		$(document.getElementsByClassName("menu")).toggleClass("active");
+		//oldLink.toggleClass("active");
+		
+		//if ((oldLink).contains("active")) {
+		//	alert(oldLink.toString);
+		//	oldLink.remove("active");
+		//}
 		var page = $(this).attr('href');
+		$(this).toggleClass("active");
 		window.scrollTo(0, 0);
 		$('.pusher').load('./pages/' + page + '.html');
 		return false;
