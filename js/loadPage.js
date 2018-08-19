dfd150c45d799a11b378b157b9b66d24160e8f9d
von Schappler <vonschappler.gaming@users.noreply.github.com>
2018-08-19 19:37:56 -0300

    update

--- js/loadPage.js
+++ js/loadPage.js
@@ -5,14 +5,18 @@ $(document).ready(function() {
 	
 	//load page by click
 	$('.item').click(function() {
-		var page = $(this).attr('href');
 		window.scrollTo(0, 0);
-		$('.pusher').fadeOut(200, function() {
+		var page = $(this).attr('href');
+		$('.pusher').fadeOut(500, function() {
+			
 			$('.pusher').hide().load('./pages/' + page + '.html', function() {
-				$('.pusher').fadeIn(200);
+				
+				$('.pusher').fadeIn(500);
+				
 			});
 		});
-	});
+	
 		//load('./pages/' + page + '.html');
 		return false;
+	});
 });
