3755e2930d7b34e003936c7103d0a401b7a238c1
von Schappler <vonschappler.gaming@users.noreply.github.com>
2018-08-22 23:51:29 -0300

    Teste

--- js/main.js
+++ js/main.js
@@ -13,13 +13,8 @@ $(document).ready(function() {
 	
 	// create sidebar and attach to menu open
 	$('.ui.sidebar').sidebar('attach events', '.toc.item');
-<<<<<<< HEAD
 	$('.ui.embed').embed( {
 		play: false,
 		replay: true
 	});
-=======
-	$('.ui.embed').embed();
-	
->>>>>>> 21c4b12ce1e02b60dea9814b426c1d1c3002d86d
 });
