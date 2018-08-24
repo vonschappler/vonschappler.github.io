e69ba86618d906dd3cdf57771c749cef3cd3ef95
von Schappler <vonschappler.gaming@users.noreply.github.com>
2018-08-23 21:18:35 -0300

    Bug Fixes e atualizacao games.csv

--- js/loadPage.js
+++ js/loadPage.js
@@ -1,38 +1,4 @@
 // JavaScript Document
-function mkGameList() {
-	$.ajax({
-			url:"../txt/games.csv",
-			dataType:"text",
-			success:function(data)
-			{
-				var employee_data = data.split(/\r?\n|\r/);
-				var table_data = '<table id="gameList" class="ui celled table"><thead>';
-				for(var count = 0; count<employee_data.length; count++){
-					var cell_data = employee_data[count].split(",");
-					table_data += '<tr>';
-					for(var cell_count=0; cell_count<cell_data.length; cell_count++){
-						if(count === 0){
-							if (cell_count == 0) {
-							table_data += '<th class="sorted ascending">'+cell_data[cell_count]+'</th>';
-							} else {
-								table_data += '<th>'+cell_data[cell_count]+'</th>';
-							}
-						}
-
-						else {
-							table_data += '<td>'+cell_data[cell_count]+'</td>';
-						}
-					}
-					table_data += '</tr>';
-				}
-				table_data += '</table>';
-				
-				$('#tableLoad.content').html(table_data);
-			}
-		});
-}
-
-
 function showLibGame() {
 	
 	$('#game')
