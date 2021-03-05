62681182b3d783a2f8345b63e8e6ece80f5de4d5
Manoel Viturino <vonschappler.gaming@gmail.com>
2021-03-05 16:59:07 -0300

    update website - mar/2021

--- js/loadPage.js
+++ js/loadPage.js
@@ -24,19 +24,40 @@ function showModal(a) {
 
 function loadPage(a) {
 	var newUrl = '/pages/' + a + '.html';
+	check = a.split("/")
+	lang = check[0]
+	page = check[1]
+	console.log(lang)
+	console.log(page)
 	window.scrollTo(0, 0);
 	$('.pusher').fadeOut(1000, function() {
 		$('.pusher').hide().load(newUrl, function() {
 			$('.pusher').fadeIn(1000);
 		});
-		
 		$('.ui.embed').embed();
 		return true;
 	});
+	const link = new URL(window.location)
+	link.searchParams.set('display', page)
+	link.searchParams.set('lang', lang)
+	console.log(link)
+	window.history.pushState({}, '', link);
 }
 
 function goHome() {
 	var url = 'http://rebrand.ly/vonWebsite';
 	var win = window.open(url, '_blank');
 	win.focus();
+}
+
+function setAddress() {
+	lang = 'en'
+	page = 'index'
+	console.log(lang)
+	console.log(page)
+	const link = new URL(window.location)
+	link.searchParams.set('display', page)
+	link.searchParams.set('lang', lang)
+	console.log(link)
+	window.history.pushState({}, '', link);
 }
